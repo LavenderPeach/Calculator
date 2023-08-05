7b5eae82fa7459cf076f59265382e345d061c413
Cade <134173296+LavenderPeach@users.noreply.github.com>
2023-08-05 12:34:42 -0400

    Fix error message when dividing by zero
    Changed alert message that popped up when dividing by zero and made it appear on the calculator screen instead.

--- script.js
+++ script.js
@@ -21,30 +21,34 @@ function convertOperator(keyboardOperator) {
     if (keyboardOperator === '/') return '÷'
 }
 
-function operate (operator, a, b) {
-    a === Number(a)
-    b === Number(b)
+function operate(operator, a, b) {
+    a = parseFloat(a); // Convert to a floating-point number
+    b = parseFloat(b); // Convert to a floating-point number
+
     switch (operator) {
         case '+':
-            return add(a,b);
+            return add(a, b);
         case '-':
             return subtract(a, b);
         case 'x':
             return multiply(a, b);
         case '÷':
-            if (b === 0) return null
-            else return divide(a, b)
+            if (b === 0) return null;
+            else return divide(a, b);
+        default:
+            return null;
     }
 }
 
+
 const numberButtons = document.querySelectorAll('.btn-number')
 const operatorButtons = document.querySelectorAll('.btn-operator')
-const clearButton = document.getElementsByClassName('.btn-clear')
-const deleteButton = document.getElementsByClassName('.btn-delete')
-const decimalButton = document.getElementsByClassName('.btn-decimal')
-const equalsButton = document.getElementsByClassName('.btn-equal')
-const lastScreen = document.getElementsByClassName('.screen-last')
-const currentScreen = document.getElementsByClassName('.screen-current')
+const clearButton = document.querySelector('.btn-clear')
+const deleteButton = document.querySelector('.btn-delete')
+const decimalButton = document.querySelector('.btn-decimal')
+let equalsButton = document.querySelector('.btn-equal')
+const lastScreen = document.querySelector('.screen-last')
+const currentScreen = document.querySelector('.screen-current')
 
 let firstOperand = ''
 let secondOperand = ''
@@ -60,15 +64,16 @@ button.addEventListener('click', () => setOperation(button.textContent))
 )
 
 window.addEventListener('keydown', handleKeyboardInput)
-equalsButton.addEventListener('click', eval)
+equalsButton.addEventListener('click', evaluate)
 clearButton.addEventListener('click', clear)
 deleteButton.addEventListener('click', deleteNumber)
 decimalButton.addEventListener('click', appendDecimal)
 
 function appendNumber(number) {
-    if (currentScreen.textContent === '0' || shouldResetScreen)
+    if (currentScreen.textContent === '0' || shouldResetScreen){
     resetScreen()
-    currentScreen.textContent += number
+    }
+    currentScreen.textContent += number 
 }
 
 function resetScreen() {
@@ -97,23 +102,53 @@ currentScreen.textContent = currentScreen.textContent
 }
 
 function setOperation(operator) {
-    if (currentOperation !== null) eval()
+    if (currentOperation !== null) evaluate()
     firstOperand = currentScreen.textContent
     currentOperation = operator
     lastScreen.textContent = `${firstOperand} ${currentOperation}`
     shouldResetScreen = true
 }
 
-function eval() {
-    if (currentOperation === null || shouldResetScreen) return
+function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return;
+
     if (currentOperation === '÷' && currentScreen.textContent === '0') {
-        alert('Error!')
-        return
+        currentScreen.textContent = 'Error';
+        return;
     }
-    secondOperand = currentScreen.textContent
-    currentScreen.textContent = roundResult(
-        operate(firstOperand, currentOperation, secondOperand)   
-    )
-    lastScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperation} =`
-    currentOperation = null
+
+    secondOperand = currentScreen.textContent;
+
+    // Perform the calculation using operate() function
+    let result = operate(currentOperation, firstOperand, secondOperand);
+
+    // Check for valid result
+    if (result === null || isNaN(result)) {
+        alert('Error: Invalid calculation!');
+        return;
+    }
+
+    // Update the screen with the rounded result
+    currentScreen.textContent = roundResult(result);
+
+    // Update the lastScreen with the full expression
+    lastScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+
+    // Reset the currentOperation and shouldResetScreen
+    currentOperation = null;
+    shouldResetScreen = true;
+}
+
+function roundResult(result) {
+    return Math.round(result * 10000) / 10000;
+}
+
+function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+    if (e.key === '.') appendDecimal()
+    if (e.key === '=' || e.key === 'Enter') evaluate()
+    if (e.key === 'Delete') deleteNumber()
+    if (e.key === 'Escape') clear ()
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+        setOperation(convertOperator(e.key))
 }
